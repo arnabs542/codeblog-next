f18fc3db5897a1219ec5054818d10697a9215495
Ashish Ranjan <ashish.ranjan@payu.in>
2022-02-12 00:10:07 +0530

    added blog code

--- pages/contact.js
+++ pages/contact.js
@@ -1,7 +1,18 @@
-export default function Contact() {
+import { Fragment } from 'react';
+import Head from 'next/head';
+
+import ContactForm from '../components/contact/contact-form';
+
+function ContactPage() {
   return (
-    <div>
-      
-    </div>
-  )
+    <Fragment>
+      <Head>
+        <title>Contact Me</title>
+        <meta name='description' content='Send me your messages!' />
+      </Head>
+      <ContactForm />
+    </Fragment>
+  );
 }
+
+export default ContactPage;
